5eb99f57a9b4a50d9ceb2f933950abb8d64fd676
unknown <esinamseraphine7@gmail>
2024-12-09 06:45:12 -0800

    adding all files

---
+++ lesson2/src/index.js
@@ -0,0 +1,12 @@
+alert(console.log),
+console.log(6);
+let num2=2;
+console.log(num2);
+let num3=13;
+console.log(num3);
+let result=(num3+2),
+let job =("mining"),
+console.log(`my dream job is ${job}`);
+
+
+
---
+++ lesson4/src/index.js
@@ -0,0 +1,38 @@
+let x=prompt("give me a number");
+console.log(typeof{x});
+x=number(x);
+console.log(typeof{x});
+
+let result=(x+15);
+let remainder=result%2;
+if(remainder(1)==1);
+let booking=prompt("let us know if you to cancel,confrim,or reshedule");
+
+let num1=3;
+let num2=5;
+let result2=num3 + num4;
+console.log(result2);
+function addTwoNumbers(no1,no2){
+    alert(``)
+}
+
+
+function CookRice(rice,salt,water){
+    alert(`I COOKED${rice}cups of rice with${salt},a pinch of salt,  with ${}`)
+    CookRice("half", "500ml", 10);
+    // let many=prompt
+    
+}
+function checkAge(){
+    let age=prompt("what is your age?");
+    if(age < 6){
+        alert(`you are ${age}years old and you are too young.`)
+    } else if(age >= 6 && age <= 17){
+        alert(`you are ${age} years old and you have limited options.` )
+    }else if(age && age >= 6 && age <= 18){
+
+    }alert(`you arg${age}years old you have limited option.`)
+
+    }
+
+    
